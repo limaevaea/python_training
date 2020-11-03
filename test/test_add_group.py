4940250ce5fcf36e34c2825344cbefe9d1c9d3f5
limaevaea <limaevaea@gmail.com>
2020-11-03 21:38:16 +0300

    выделен класс для работы с группами(декомпозиция)

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,12 +13,12 @@ def app(request):
 
 def test_add_new_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="NewTest01", header="Testing1", footer="Test"))
+    app.group.create(Group(name="NewTest01", header="Testing1", footer="Test"))
     app.session.logout()
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 
 
