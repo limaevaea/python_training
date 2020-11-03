c035c2e9c5c2fadc7482f63fc9c2e851e4e46931
limaevaea <limaevaea@gmail.com>
2020-11-03 20:22:05 +0300

    add_group Созданы пакеты

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
-import unittest
 import pytest
-from group import Group
-from application import Application
+from model.group import Group
+from fixture.application import Application
 
 
 @pytest.fixture
@@ -22,13 +21,5 @@ def test_add_empty_group(app):
     app.create_group(Group(name="", header="", footer=""))
     app.logout()
 
-    """    def is_element_present(self, how, what):
-        try: self.wd.find_element(by=how, value=what)
-        except NoSuchElementException as e: return False
-        return True
-    
-    def is_alert_present(self):
-        try: self.wd.switch_to_alert()
-        except NoAlertPresentException as e: return False
-        return True"""
+
 
