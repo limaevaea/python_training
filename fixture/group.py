b7abbedcf1ed37ce569d1eb12aa8655420854f82
limaevaea <limaevaea@gmail.com>
2020-11-09 19:32:53 +0300

    Оптимизация ввода данных - группы

--- fixture/group.py
+++ fixture/group.py
@@ -30,17 +30,18 @@ class GroupHelper:
         # submit edit group
         wd.find_element_by_name("update").click()
 
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
+
     def fill_group_form(self, group):
         wd = self.app.wd
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
 
 
     def return_to_groups_page(self):
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,5 +3,5 @@ from model.group import Group
 
 def test_edit_first_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name="Edit Name", header="Edit header", footer="Edit footer"))
+    app.group.edit_first_group(Group(name="Edit Name"))
     app.session.logout()
