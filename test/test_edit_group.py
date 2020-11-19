d651036bb4f68bc286544f4a78219b2831eff171
limaevaea <limaevaea@gmail.com>
2020-11-19 21:29:33 +0300

    Рандом в тесты групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,13 +1,15 @@
 from model.group import Group
+from random import randrange
 
 def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Edit Name"))
     old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
     group = Group(name="Edit Name")
-    group.id = old_groups[0].id
-    app.group.edit_first_group(group)
+    group.id = old_groups[index].id
+    app.group.edit_some_group(index, group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
