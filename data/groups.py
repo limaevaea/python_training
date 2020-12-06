9390894338d43a13ed09c1ef73f8358b0eeb503a
limaevaea <limaevaea@gmail.com>
2020-12-06 23:36:41 +0300

    "внедрение" тестовых данных в фикстуру при помощи hook-функции pytest_generate_tests.

---
+++ data/groups.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from model.group import Group
+
+
+testdata = [
+    Group(name="name1", footer="footer1", header="header1"),
+    Group(name="name2", footer="footer2", header="header2")
+]
+
--- generator/contact.py
+++ generator/contact.py
@@ -22,7 +22,6 @@ for o,a in opts:
     elif o == "-f":
         f = a
 
-
 def random_string(prefix, maxlen):
     symbols = string.ascii_letters + string.digits #+ " "*10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
