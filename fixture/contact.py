67fd3131828a10c43d7bfe6fc2f1632dbfb22fe4
limaevaea <limaevaea@gmail.com>
2020-12-14 00:50:05 +0300

    тесты контактов(delete) списки из БД

--- fixture/contact.py
+++ fixture/contact.py
@@ -39,6 +39,19 @@ class ContactHelper:
         wd.find_element_by_xpath("//div[@id='content']/div").click()
         self.contact_cache = None
 
+    def select_contact_by_id(self, id):
+        wd = self.app.wd
+        self.open_contact_page()
+        wd.find_element_by_css_selector("input[value='%s']" % id).click()
+
+    def delete_contact_by_id(self, id):
+        wd = self.app.wd
+        self.select_contact_by_id(id)
+        wd.find_element_by_xpath("(//input[@value='Delete'])").click()
+        wd.switch_to_alert().accept()
+        wd.find_element_by_xpath("//div[@id='content']/div").click()
+        self.contact_cache = None
+
     def edit_some_contact(self, index, contact):
         wd = self.app.wd
         self.open_contact_page()
--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,13 +1,13 @@
 from model.сontact import Сontact
-from random import randrange
+import random
 
-def test_delete_first_contact(app):
-    if app.contact.count() == 0:
+def test_delete_contact(app, db):
+    if len(db.get_contact_list()) == 0:
         app.contact.create_new_contact(Сontact(firstname="AddNewContact"))
-    old_contacts = app.contact.get_contact_list()
-    index = randrange(len(old_contacts))
-    app.contact.delete_some_contact(index)
-    new_contacts = app.contact.get_contact_list()
+    old_contacts = db.get_contact_list()
+    contact =random.choice(old_contacts)
+    app.contact.delete_contact_by_id(contact.id) #переделать метод новый
+    new_contacts = db.get_contact_list()
     assert len(old_contacts) - 1 == len(new_contacts)
-    old_contacts[index:index+1] = [] #вырезка от 0 до 1 не вкл 1
+    old_contacts.remove(contact) #вырезка от 0 до 1 не вкл 1
     assert old_contacts == new_contacts
